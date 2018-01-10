b88b3483cfbd2a03a6cd6bd95fa87a13867a2a7c
Henrique Fernandes <henriquedin@hotmail.com>
2018-01-10 00:17:12 -0200

    Funções recursivas
    Utilização de funções recursivas

---
+++ funcoes/exemplo07.php
@@ -0,0 +1,19 @@
+<?php
+
+function soma(int ...$valores){
+
+	return array_sum($valores);
+
+}
+
+echo soma(2,2);
+echo '<br>';
+
+echo soma(25,33);
+echo '<br>';
+
+
+echo soma(2.5,1.4);
+echo '<br>';
+
+?>
---
+++ funcoes/exemplo09.php
@@ -0,0 +1,83 @@
+<?php
+
+$hierarquia = array(
+
+	array(
+		'nome_cargo' => 'CEO',
+		'subordinados' => array(
+			//Inicio: Diretor Comercial
+			array(
+				'nome_cargo' => 'Diretor Comercial',
+				'subordinados'=> array(
+					//Início: Gerente de vendas
+					'nome_cargo'=>'Gerente de Vendas'
+				)
+				//Término: Gerente de vendas
+			),
+			//Término: Diretor Comercial 
+			
+			//Início: Diretor Financeiro
+			array(
+				'nome_cargo' => 'Diretor Financeiro',
+				'subordinados'=> array(
+				//Início: Gerente de Contas a Pagar
+				array(
+					'nome_cargo'=>'Gerente de Contas a Pagar',
+					'subordinados'=> array(
+					//Início: Supervisor de pagamentos
+					array(
+						'nome_cargo'=>'Supervisor de Pagamentos'
+					)
+					//Término: Supervisor de Pagamentos
+				)
+			),
+			//Término: Gerente de Contas a Pagar	
+			//Início: Gerente de Compras
+			array(
+				'nome_cargo'=>'Gerente de Compras',
+				'subordinados'=>array(
+					//Início: Supervisor de Suprimentos
+						array(
+							'nome_cargo'=>'Supervisor de Suprimentos'
+							)
+					//Término: Supervisor de Suprimentos
+						)
+				//Término: Gerente de Compras
+					)
+				//Término: Diretor Financeiro 
+				)
+			)	
+		)
+	)
+);
+
+
+function exibe($cargos){
+
+	$html = "<ul>";
+
+	foreach ($cargos as $cargo) {
+	 	
+		$html .="<li>";
+
+		$html .=$cargo['nome_cargo'];
+
+		if(isset($cargo['subordinados']) && count($cargo['subordinados']) > 0){
+
+			$html .= exibe($cargo['subordinados']);
+
+		}
+
+		$html .="</li>";
+
+	 } 
+	
+	$html .= "</ul>";
+
+	return $html;
+
+}
+
+echo exibe($hierarquia);
+
+?>
