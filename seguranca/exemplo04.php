bcf528239bbc8f02444ed1ee1f786cd1bb8cf7b3
Henrique Fernandes <henry1991.contato@gmail.com>
2018-03-02 20:51:06 -0300

    reCAPTCHA
    Utilização do recaptcha para impedir robôs.

---
+++ seguranca/exemplo04.php
@@ -0,0 +1,10 @@
+
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<form action="cadastrar.php" method="POST">
+	
+	<div class="g-recaptcha" data-sitekey="6LdwKUoUAAAAAHqopYiHzEheWuhvgFRyFTFyORxC"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+
+</form>
